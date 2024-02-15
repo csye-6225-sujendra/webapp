fe4b97d08e0c9c6138e63be6f0fe32d9f9d64982
Sujendra Gharat <sgharat297@gmail.com>
2024-02-14 21:57:19 -0500

    testing workflow

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../../server');
 const db = require('../../models/models');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 // Test data
 const testUser = {
