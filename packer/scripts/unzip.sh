97ee4dfbdb6278b7bb92fbf3e03637e76f43c6a8
Sujendra Gharat <sgharat297@gmail.com>
2024-02-21 11:51:53 -0500

    testing

--- packer/scripts/unzip.sh
+++ packer/scripts/unzip.sh
@@ -2,10 +2,14 @@
 
 echo "Installing unzip package..."
 sudo dnf -y install unzip
+
 echo "Unzipping webapp.zip..."
 unzip /tmp/webapp.zip -d /tmp/
+
 echo "Changing ownership of /tmp/webapp to csye6225:csye6225..."
 sudo chown csye6225:csye6225 /tmp/webapp
+
 echo "Changing permissions of /tmp/webapp to make it executable..."
 sudo chmod +x /tmp/webapp/server.js
+
 echo "Script execution completed."
