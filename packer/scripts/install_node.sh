ba67caa9088666de55534d4c13e114ddf75ce5af
Sujendra Gharat <sgharat297@gmail.com>
2024-02-21 19:45:33 -0500

    testing

--- packer/scripts/install_node.sh
+++ packer/scripts/install_node.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Download Node.js version 18.15.0
-curl -o- https://nodejs.org/dist/v18.15.0/node-v18.15.0-linux-x64.tar.xz | tar xJ
+# # Download Node.js version 18.15.0
+# curl -o- https://nodejs.org/dist/v18.15.0/node-v18.15.0-linux-x64.tar.xz | tar xJ
+
+# # Move the Node.js files to /usr/local
+# sudo mv node-v18.15.0-linux-x64 /usr/local/node-v18.15.0
+
+# # Add Node.js to the system PATH
+# echo 'export PATH=/usr/local/node-v18.15.0/bin:$PATH' >> ~/.bashrc
+
+# # Apply the changes to the current shell
+# source ~/.bashrc
+
+# node -v
+
+# Download Node.js version 21.4.0
+curl -o- https://nodejs.org/dist/v18.15.0/node-v18.15.0-linux-x64.tar.xz | tar -xJ
 
 # Move the Node.js files to /usr/local
 sudo mv node-v18.15.0-linux-x64 /usr/local/node-v18.15.0
 
 # Add Node.js to the system PATH
-echo 'export PATH=/usr/local/node-v18.15.0/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/usr/local/node-v18.15.0/bin:$PATH' | sudo tee -a ~/.bashrc
 
 # Apply the changes to the current shell
 source ~/.bashrc
